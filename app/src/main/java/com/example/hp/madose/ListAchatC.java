d75552b36489bbffabf085ffcd9769e82676f4c8
erick <erickouadjo7@gmail.com>
2018-02-05 14:04:35 +0000

    eric008

---
+++ app/src/main/java/com/example/hp/madose/ListAchatC.java
@@ -0,0 +1,37 @@
+package com.example.hp.madose;
+
+/**
+ * Created by erick on 02/02/2018.
+ */
+
+public class ListAchatC {
+    String LibBes;
+    int Qte;
+
+    public ListAchatC(String libBes, int qte) {
+        LibBes = libBes;
+        Qte = qte;
+    }
+
+    public String getLibBes() {
+        return LibBes;
+    }
+
+    public void setLibBes(String libBes) {
+        LibBes = libBes;
+    }
+
+    public int getQte() {
+        return Qte;
+    }
+
+    public void setQte(int qte) {
+        Qte = qte;
+    }
+
+    @Override
+    public String toString() {
+        return  LibBes + "\n"+
+                "Qte=" + Qte;
+    }
+}
