89bc8cf68f3e50560c72a18455fcdcc792a5fa48
HP <adepatrickade@gmail.com>
2018-02-08 16:15:11 +0000

    Paco9

--- app/src/main/java/com/example/hp/madose/Add.java
+++ app/src/main/java/com/example/hp/madose/Add.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
@@ -43,7 +44,7 @@ public class Add extends AppCompatActivity {
         final EditText qte=(EditText)findViewById(R.id.QT);
         final EditText mark=(EditText)findViewById(R.id.marq);
         final EditText autre=(EditText)findViewById(R.id.autre);
-        final ImageButton button= findViewById(R.id.boutoun);
+        final ImageView button= findViewById(R.id.boutoun);
 
 
 
@@ -167,7 +168,7 @@ public class Add extends AppCompatActivity {
 
                 }
 
-                else if (!nf.contains(four.getText().toString())){
+                else if (!nf.contains(four.getText().toString()) && !four.getText().toString().equals("")){
                     four.setError("Le fournisseur choisi ne fait pas partie de la liste des fournisseurs de l'entreprise.\n Veuillez donc l'ajouter à la liste préalablement en cliquant sur le bouton + \nsinon veuillez entrer un fournisseur faisant partie de la liste.");
                     button.setVisibility(View.VISIBLE);
                     button.setOnClickListener(new View.OnClickListener() {
@@ -180,7 +181,7 @@ public class Add extends AppCompatActivity {
                 }
                 else if (four.getText().toString().equals(""))
                 {
-                    Toast.makeText(getBaseContext(),"Veuillez saisir le nom du fournisseur de cette approvisionnement SVP!!",Toast.LENGTH_LONG).show();
+                    four.setError("Veuillez saisir le nom du fournisseur de cette approvisionnement SVP!!");
                     four.requestFocus();
                 }
                 else if (besoin.getText().toString().equals(""))
