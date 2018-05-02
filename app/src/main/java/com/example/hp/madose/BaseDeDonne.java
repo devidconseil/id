9daa58753bf1f6a3f44ea55eebec156edd715dc8
HP <adepatrickade@gmail.com>
2018-05-02 14:56:00 +0000

    RecapApp v18.05.02

--- app/src/main/java/com/example/hp/madose/BaseDeDonne.java
+++ app/src/main/java/com/example/hp/madose/BaseDeDonne.java
@@ -1638,7 +1638,7 @@ public ArrayList<String> affiNumDem(int idemp)
         }
     }
     public String selectStockBesDemande(String LibBes){
-        String requete="select sum(qte) from Besoin,Demande_Besoins where Demande_Besoins.numBes=Besoin.numBes and libBes='"+LibBes+"' group by qte;";
+        String requete="select sum(qte) as qte from Besoin,Demande_Besoins where Demande_Besoins.numBes=Besoin.numBes and libBes='"+LibBes+"' group by libBes;";
         Cursor cursor = null;
         try {
 
@@ -1650,7 +1650,7 @@ public ArrayList<String> affiNumDem(int idemp)
     }
 
     public String selectStockBesSortie(String LibBes){
-        String requete="select sum(qte) from Besoin,Besoins_Sortie where Besoins_Sortie.numBes=Besoin.numBes and libBes='"+LibBes+"' group by qte;";
+        String requete="select sum(qte) as qte from Besoin,Besoins_Sortie where Besoins_Sortie.numBes=Besoin.numBes and libBes='"+LibBes+"' group by libBes;";
         Cursor cursor = null;
         try {
 
@@ -1737,7 +1737,7 @@ public ArrayList<String> affiNumDem(int idemp)
 
 
        // String req="select Besoin.libBes,sum(qte) as qte from Besoin,Demande_Besoins,Demande where Besoin.NumBes=Demande_Besoins.NumBes and Demande_Besoins.numDem=Demande.numDem  group by libBes;";
-        String req="select Besoin.libBes, StockBes-(Demande_Besoins.qte - Besoins_Sortie.qte) as qte from Besoin,Besoins_Sortie,Demande_Besoins where Besoin.NumBes=Besoins_Sortie.Numbes and Besoin.NumBes=Demande_Besoins.NumBes group by libBes;";
+        String req="select Besoin.libBes, StockBes-(Demande_Besoins.qte - Besoins_Sortie.qte) as qte from Besoin,Besoins_Sortie,Demande_Besoins where Besoin.NumBes=Besoins_Sortie.Numbes or Besoin.NumBes=Demande_Besoins.NumBes group by libBes;";
         Cursor cursor=this.getReadableDatabase().rawQuery(req, null);
         cursor.moveToFirst();
 
